76f6cfa5b2f8641181cf2436c2472e6117a27e26
GUNN33RR <ghanemismail16@gmail.com>
2024-01-16 10:30:53 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const FirstName = "Ghanem";
+const LastName = "Ismail";
+let isActive = true;
+const UserId = 2112;
+
+
+console.log(FirstName);
+console.log(LastName);
+console.log(isActive);
+console.log(UserId);
+
+console.log(`User First Name:${FirstName} Last Name:${LastName} Is Active:${isActive} User ID:${UserId}`)
